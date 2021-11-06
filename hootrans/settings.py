491635f0cef48c4a019604886b9d12238a8528c9
drcall <drcall4@outlook.com>
2021-11-06 11:28:49 -0400

    Restructured using a .env

--- hootrans/settings.py
+++ hootrans/settings.py
@@ -10,20 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ioxr@yj=7jcepk_a@%$h2fquge&l5sw%5m2ta)&i15ee2=#8ie'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -76,9 +83,9 @@ WSGI_APPLICATION = 'hootrans.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hootrans-db', 
+        'NAME': env('DB_USER'), 
         'USER': 'postgres', 
-        'PASSWORD': 'yuOeucv5iydbcAzP',
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
